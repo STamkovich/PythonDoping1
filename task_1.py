d19b705c4cb3a6a7de81ac3bc9d896e874a87d3e
Sergey <80621773+STamkovich@users.noreply.github.com>
2021-03-23 12:43:19 +0300

    task_1.py task_4.py

--- task_1.py
+++ task_1.py
@@ -8,26 +8,21 @@
 # ** Написать тестовую функцию которая будет проверять логику функции.
 def function_python():
     a = 5
-    type(a)
     b = 2.0
-    type(b)
     c = 1 + 2j
-    type(c)
     e = [1, 2.2, 'python']
-    type(e)
     f = (5, 'program', 1 + 3j)
-    type(f)
     g = "Простая строка"
-    type(g)
     k = {1, 2, 3, 4, 5}
-    type(k)
-    my_dict = {"int": a,
-               "flout": b,
-               "complex": c,
-               "list": e,
-               "tuple": f,
-               "str": g,
-               "set": k}
+    my_dict = {
+            type(a): a,
+            type(b): b,
+            type(c): c,
+            type(e): e,
+            type(f): f,
+            type(g): g,
+            type(k): k
+    }
 
     for key, value in my_dict.items():
         print("Ключь: " + str(key) + " / значение: " + str(value))
